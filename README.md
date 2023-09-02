13984fdc85c784bf20a038fafbfc30a90eaca3a1
Leonardo16AM <leo16am@gmail.com>
2023-09-02 13:45:13 -0400

    Cahgned the code and added README

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# Security-Camera
-Minimal Security Camera with  movement control 
+# Raspberry-Security-Camera
+
+## Overview
+
+This repository provides a simple, efficient, and resource-light solution to set up a motion-detection security camera using a Raspberry Pi. The Python script utilizes OpenCV for video capture and motion detection.
+
+## Prerequisites
+
+1. A Raspberry Pi with Raspbian OS installed.
+2. A camera module compatible with Raspberry Pi.
+3. Python 3.x installed on the Raspberry Pi.
+
+## Dependencies
+
+The script uses the following Python libraries:
+- OpenCV
+- NumPy
+
+You can install these using the following commands:
+
+```bash
+sudo apt-get update
+sudo apt-get install python3-opencv
+```
+
+## Installation
+
+1. Clone this repository to your Raspberry Pi.
+2. Update the `save_path` variable in the `SecurityCam.py` script with the path where you'd like to save the captured images.
+
+## Running the Script
+
+To run the script manually, navigate to the repository folder and execute:
+
+```bash
+python3 SecurityCam.py
+```
+
+Press `q` to stop the script.
+
+## Auto-Start on Boot
+
+To have the script run automatically in the background upon Raspberry Pi startup, you can set up a `systemd` service.
+
+### Create a systemd Service File
+
+1. Open a new file in the `/etc/systemd/system/` directory:
+
+    ```bash
+    sudo nano /etc/systemd/system/motion_camera.service
+    ```
+
+2. Add the following content:
+
+    ```ini
+    [Unit]
+    Description=Motion Camera Service
+    After=network.target
+
+    [Service]
+    ExecStart=/usr/bin/python3 /path/to/your/SecurityCam.py
+    WorkingDirectory=/path/to/your/
+    StandardOutput=inherit
+    StandardError=inherit
+    Restart=always
+    User=pi
+
+    [Install]
+    WantedBy=multi-user.target
+    ```
+
+    Replace `/path/to/your/SecurityCam.py` with the full path to your Python script and `/path/to/your/` with the directory where the script resides.
+
+### Enable and Start the Service
+
+3. Reload the `systemd` daemon and enable the service:
+
+    ```bash
+    sudo systemctl daemon-reload
+    sudo systemctl enable motion_camera.service
+    ```
+
+4. Start the service:
+
+    ```bash
+    sudo systemctl start motion_camera.service
+    ```
+
+### Service Management
+
+- To check the service status: `sudo systemctl status motion_camera.service`
+- To stop the service: `sudo systemctl stop motion_camera.service`
+- To restart the service: `sudo systemctl restart motion_camera.service`
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+---
+
+Feel free to contribute to this project. For any issues or feature requests, please open an issue or submit a pull request.
--- SecurityCam.py
+++ SecurityCam.py
@@ -1,91 +1,45 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-import math  
-from termcolor import colored
-
-from scipy.spatial.distance import cosine
 import cv2
+import numpy as np
+from datetime import datetime
 
-cam = cv2.VideoCapture(0)
+def resize_frame(frame, scale_percent):
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-color = np.random.randint(0,255,(100,3))
+def detect_motion(video_source=0, save_path="/path/to/save/", scale_percent=50):
+    cap = cv2.VideoCapture(video_source)
+    ret, frame1 = cap.read()
+    ret, frame2 = cap.read()
 
-ret, old_frame = cam.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    frame1 = resize_frame(frame1, scale_percent)
+    frame2 = resize_frame(frame2, scale_percent)
 
-mask = np.zeros_like(old_frame)
-mask_features = np.zeros_like(old_gray)
-mask_features[:,0:20] = 1
-mask_features[:,620:640] = 1
+    while cap.isOpened():
+        diff = cv2.absdiff(frame1, frame2)
+        gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 3,
-                       blockSize = 7,
-                       mask = mask_features)
+        for contour in contours:
+            if cv2.contourArea(contour) < 500:
+                continue
 
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            cv2.imwrite(f"{save_path}{timestamp}.jpg", frame1)
+            break
 
-def init_new_features(gray_frame):
-    corners = cv2.goodFeaturesToTrack(gray_frame, **feature_params)
-    return corners
+        frame1 = frame2
+        ret, frame2 = cap.read()
+        frame2 = resize_frame(frame2, scale_percent)
 
-def calculateDistance(x1,y1,x2,y2):  
-    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
-    return dist  
-
-corners = init_new_features(old_gray)
-img_num=0
-while True:
-    try:
-        cam_moved = False
-        cam_status = None
-        ret,frame = cam.read()
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, corners, None, **lk_params)
-        
-        good_new = p1
-        good_old = corners
-        
-        for i,(new,old) in enumerate(zip(good_new,good_old)):
-            a,b = new.ravel()
-            c,d = old.ravel()
-            
-            distance = calculateDistance(a,b,c,d)
-         
-            if distance>8:
-                cam_moved = True
-                old_gray = frame_gray.copy()
-                corners = init_new_features(old_gray)
-            else:
-                old_gray = frame_gray.copy()
-                corners = good_new.reshape(-1,1,2)
-                
-        
-        if cam_moved is True:
-            it = np.random.rand(1)[0]
-            print('Camera moved '+ str(it))
-            cam_status = 'Camera moved'
-            img_num+=1
-            cv2.imwrite(f'recorded/{img_num}.jpg',frame)
-        
-        img = cv2.add(frame,mask)
-        
-        # cv2.imshow('frame',img)
-        
-        mask = np.zeros_like(old_frame)
-        if corners is None:
-            corners = init_new_features(old_gray)
-        
-        
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            cam.release()
-        
-    except TypeError as e:
-        print(colored(e,"red"))
-        break
+            break
+            
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    detect_motion(video_source=0, save_path="/path/to/save/", scale_percent=50)
